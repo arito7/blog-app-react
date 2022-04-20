191f6929c3d321a39bcd6a2c260d60e6a1aa8995
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-20 12:54:03 +0900

    Add styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@
 import { css, jsx } from '@emotion/react';
 import { useEffect, useState } from 'react';
 import { API_ENDPOINT } from '../config/constants';
+import { grey, shadow, shadowDarker } from '../config/css';
+import moment from 'moment';
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
@@ -22,11 +24,15 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="postContainer">
+    <div>
       <ul
         css={css`
+          margin: auto;
           display: grid;
           gap: 1rem;
+          max-width: 85%;
+          padding: 1rem;
+          justify-contents: center;
         `}
       >
         {posts
@@ -44,18 +50,36 @@ const Post = ({ post }) => {
     <li
       css={css`
         color: blue;
+        padding: 0 1rem;
+        background-color: ${grey};
+        border-radius: 0.25rem;
+        box-shadow: ${shadow};
+        transition-property: box-shadow;
+        transition-duration: 300ms;
         &:hover {
-          color: red;
+          box-shadow: ${shadowDarker};
         }
       `}
     >
-      <h2
+      <div
         css={css`
-          color: blue;
+          padding: 0 1rem;
+          display: grid;
+          grid-template-columns: 1fr auto;
+          justify-items: center;
+          align-items: center;
         `}
       >
-        {post.title}
-      </h2>
+        <h2
+          css={css`
+            justify-self: start;
+          `}
+        >
+          {post.title}
+        </h2>
+        <span>{moment(post.createdAt).format('MMM d, YYYY')}</span>
+      </div>
+
       <p>{post.body}</p>
     </li>
   );

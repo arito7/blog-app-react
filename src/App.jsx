031c8595bfef37435757e49d675a4d70abbeb5db
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-20 19:05:45 +0900

    Add container for main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
 import './App.css';
 import { Link, Outlet } from 'react-router-dom';
-import ResponsiveAppBar from './components/ResponsiveAppbar';
+import Appbar from './components/Appbar';
+
 function App() {
   return (
     <div className="App">
-      <ResponsiveAppBar />
-      <Outlet />
+      <Appbar />
+      <section
+        css={css`
+          max-width: 960px;
+          margin: auto;
+        `}
+      >
+        <Outlet />
+      </section>
     </div>
   );
 }

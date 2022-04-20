8a0f444ecd90198b408b9953edf0e843bbf326d5
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-20 23:14:20 +0900

    Add Signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,120 @@
-import Form from './Form';
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+import { Box, Button, Grid, TextField, Card } from '@mui/material';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { saveJwtToLocal, signup } from '../config/helpers';
+import Loading from './Loading';
 
 const Signup = () => {
+  const loading = Loading();
+  const [errMsg, setErrMsg] = useState('');
+  const navigate = useNavigate();
+
+  const onSignup = (username, password, rpassword) => {
+    loading.setLoading(true);
+    signup(username, password, rpassword)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          loading.setLoading(false);
+          saveJwtToLocal(data.token);
+          navigate.push('/');
+        } else {
+          loading.setLoading(false);
+          setErrMsg(data.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
-    <div>
-      <Form />
+    <div
+      css={css`
+        padding: 1rem;
+      `}
+    >
+      {loading.component}
+      {errMsg ? (
+        <p
+          css={css`
+            background-color: #ffadad;
+            color: #9b0505;
+            border-radius: 0.25rem;
+            margin: 1rem;
+            padding: 1rem;
+          `}
+        >
+          {errMsg}
+        </p>
+      ) : null}
+      <Form onSignup={onSignup} />
+      <div>
+        <p>
+          Already a member? <Link to="/login">Login here</Link>
+        </p>
+      </div>
     </div>
+  );
+};
+
+const Form = ({ onSignup }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rpassword, setRpassword] = useState('');
+
+  return (
+    <Box
+      component="form"
+      action=""
+      css={css`
+        display: grid;
+        gap: 1rem;
+        padding: 1rem;
+      `}
+    >
+      <TextField
+        label="Username"
+        name="username"
+        value={password}
+        onChange={(v) => {
+          setPassword(v.target.value);
+        }}
+      />
+
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        value={username}
+        onChange={(v) => {
+          setUsername(v.target.value);
+        }}
+      />
+
+      <TextField
+        label="Repeat Password"
+        type="password"
+        value={rpassword}
+        onChange={(e) => {
+          setRpassword(e.target.value);
+        }}
+      />
+
+      <Button
+        type="button"
+        variant="outlined"
+        onClick={() => {
+          onSignup(username, password, rpassword);
+        }}
+      >
+        Signup
+      </Button>
+    </Box>
   );
 };
 
--- src/config/helpers.js
+++ src/config/helpers.js
@@ -1,5 +1,7 @@
 import { API_ENDPOINT } from './constants';
 
+const jwtKey = 'jwtKey';
+
 export const signin = (username, password) => {
   const res = fetch(`${API_ENDPOINT}/login`, {
     method: 'POST',
@@ -8,4 +10,22 @@ export const signin = (username, password) => {
   });
 
   return res;
+};
+
+export const signup = (username, password, rpassword) => {
+  const res = fetch(`${API_ENDPOINT}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password, rpassword }),
+  });
+
+  return res;
+};
+
+export const saveJwtToLocal = (jwt) => {
+  try {
+    localStorage.setItem(jwtKey, jwt);
+  } catch (error) {
+    console.log(error.message);
+  }
 };

d1468618f9d8a2745c899f4fdf2dc72c7d935fd6
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-21 13:00:32 +0900

    Convert Post to MUI card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,49 +3,21 @@ import { css, jsx } from '@emotion/react';
 import { grey, shadow, shadowDarker } from '../config/css';
 import { Link, useNavigate } from 'react-router-dom';
 import { formatDate } from '../config/helpers';
+import { Typography, Card, CardContent, CardHeader } from '@mui/material';
 
 const Post = ({ post }) => {
   const navigate = useNavigate();
   return (
-    <li
-      css={css`
-        cursor: pointer;
-        color: blue;
-        padding: 0 1rem;
-        background-color: ${grey};
-        border-radius: 0.25rem;
-        box-shadow: ${shadow};
-        transition-property: box-shadow;
-        transition-duration: 300ms;
-        &:hover {
-          box-shadow: ${shadowDarker};
-        }
-      `}
+    <Card
       onClick={() => {
         navigate(`/post/${post._id}`);
       }}
     >
-      <div
-        css={css`
-          padding: 0 1rem;
-          display: grid;
-          grid-template-columns: 1fr auto;
-          justify-items: center;
-          align-items: center;
-        `}
-      >
-        <h2
-          css={css`
-            justify-self: start;
-          `}
-        >
-          {post.title}
-        </h2>
-        <span>{formatDate(post.createdAt)}</span>
-      </div>
-
-      <p>{post.body}</p>
-    </li>
+      <CardHeader title={post.title} subheader={formatDate(post.createdAt)} />
+      <CardContent>
+        <Typography variant="body2">{post.body}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 

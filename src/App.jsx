abb196558295f8b113cafefa4695d0a632e63161
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-23 08:37:22 +0900

    Refactor out auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from '@emotion/react';
-import { Routes, Route, Outlet, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Appbar from './components/Appbar';
-import React, { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import NewPost from './pages/NewPost';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Post from './pages/Post';
 import User from './pages/User';
-import { signin, saveJwtToLocal, signout } from './config/helpers';
+import { Box } from '@mui/material';
 import { SnackbarProvider } from './contexts/SnackbarContext';
+import { AuthProvider, RequireAuth } from './contexts/AuthContext';
 
 function App() {
   return (
@@ -48,69 +48,20 @@ function App() {
 const Layout = () => {
   return (
     <SnackbarProvider>
-    <Box textAlign="center">
-      <Appbar />
-      <Box
-        component="section"
-        css={css`
-          max-width: 960px;
-          margin: auto;
-        `}
-      >
-        <Outlet />
+      <Box textAlign="center">
+        <Appbar />
+        <Box
+          component="section"
+          css={css`
+            max-width: 960px;
+            margin: auto;
+          `}
+        >
+          <Outlet />
+        </Box>
       </Box>
-    </Box>
     </SnackbarProvider>
   );
 };
-
-const AuthContext = React.createContext(null);
-
-function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-
-  const signIn = (username, password, cb) => {
-    signin(username, password)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.success) {
-          saveJwtToLocal(data.token);
-          setUser(data.user);
-          cb();
-        } else {
-          console.log(data.message);
-          cb();
-        }
-      })
-      .catch((err) => console.log(err.message));
-    cb();
-  };
-
-  const signOut = (cb) => {
-    signout();
-    setUser(null);
-    cb();
-  };
-
-  const value = { user, signIn, signOut };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
-
-export function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-function RequireAuth({ children }) {
-  const auth = useAuth();
-  const location = useLocation();
-
-  if (!auth.user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
-  return children;
-}
 
 export default App;
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,58 @@
+import React, { createContext, useState } from 'react';
+import { signin, saveJwtToLocal, signout } from '../config/helpers';
+import { useLocation, Navigate } from 'react-router-dom';
+
+interface AuthContextType {
+  user: any;
+  signIn: (username: string, password: string, cb: VoidFunction) => void;
+  signOut: (cb: VoidFunction) => void;
+}
+
+const AuthContext = createContext<AuthContextType>(null!);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState(null);
+
+  const signIn = (username: string, password: string, cb: VoidFunction) => {
+    signin(username, password)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          saveJwtToLocal(data.token);
+          setUser(data.user);
+          cb();
+        } else {
+          console.log(data.message);
+          cb();
+        }
+      })
+      .catch((err) => console.log(err.message));
+    cb();
+  };
+
+  const signOut = (cb: VoidFunction) => {
+    signout();
+    setUser(null);
+    cb();
+  };
+
+  const value = { user, signIn, signOut };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+export function RequireAuth({ children }: { children: React.ReactNode }) {
+  const auth = useAuth();
+  const location = useLocation();
+
+  if (!auth?.user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}

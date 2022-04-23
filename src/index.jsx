0009ece607932918c86b3bd9aa87229a79baa5dd
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-23 12:01:08 +0900

    Enable dark mode

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import './index.css';
 
 const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
   typography: {
     fontFamily: 'Ubuntu',
   },
@@ -15,6 +18,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <CssBaseline />
         <App />
       </BrowserRouter>
     </ThemeProvider>

10d33e3a05437a1076d1f0549f96c53c6519b33b
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-24 09:48:44 +0900

    Implement post publishing

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from '@emotion/react';
 import { Link, useNavigate } from 'react-router-dom';
-import { formatDate } from '../config/helpers';
+import { formatDate, updatePost } from '../config/helpers';
 import {
   Typography,
   Button,
@@ -17,28 +17,39 @@ import { Delete } from '@mui/icons-material';
 const Post = ({ post }) => {
   const auth = useAuth();
   const navigate = useNavigate();
+
+  const handlePublish = () => {
+    updatePost(post._id, null, null, true)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <Card
-      onClick={() => {
-        navigate(`/post/${post._id}`);
-      }}
-    >
-      <CardActionArea>
+    <Card>
+      <CardActionArea
+        onClick={() => {
+          navigate(`/post/${post._id}`);
+        }}
+      >
         <CardHeader title={post.title} subheader={formatDate(post.createdAt)} />
         <CardContent>
-          <Typography variant="body2">{post.body}</Typography>
+          <Typography variant="body2" textAlign="left">
+            {post.body.split(' ').slice(0, 50).join(' ')}
+            {post.body.length > 50 ? '...' : null}
+          </Typography>
         </CardContent>
       </CardActionArea>
-      {auth?.user ? (
+      {auth?.user?._id === post.creator ? (
         <CardActions>
-          {auth.user._id === post.creator ? (
-            <>
-              <Button color="error" endIcon={<Delete />}>
-                Delete
-              </Button>
-              <Button>Publish</Button>
-            </>
-          ) : null}
+          <Button color="error" endIcon={<Delete />}>
+            Delete
+          </Button>
+          {post.published ? null : (
+            <Button onClick={handlePublish}>Publish</Button>
+          )}
         </CardActions>
       ) : null}
     </Card>

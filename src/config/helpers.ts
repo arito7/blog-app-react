ff0ea60eae50354c6d0b4e107a751b1b54b9b2c8
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-24 09:48:23 +0900

    Conver to ts

--- src/config/helpers.ts
+++ src/config/helpers.ts
@@ -1,11 +1,14 @@
 import { API_ENDPOINT } from './constants';
 import moment from 'moment';
 
-const jwtKey = 'jwtKey';
+const jwtKey: string = 'jwtKey';
 
-const Authorization = `Bearer ${getToken()}`;
+const Authorization: string = `Bearer ${getToken()}`;
 
-export const signin = (username, password) => {
+export const signin = (
+  username: string,
+  password: string
+): Promise<Response> => {
   const res = fetch(`${API_ENDPOINT}/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -15,7 +18,11 @@ export const signin = (username, password) => {
   return res;
 };
 
-export const signup = (username, password, rpassword) => {
+export const signup = (
+  username: string,
+  password: string,
+  rpassword: string
+): Promise<Response> => {
   const res = fetch(`${API_ENDPOINT}/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -25,10 +32,10 @@ export const signup = (username, password, rpassword) => {
   return res;
 };
 
-export const saveJwtToLocal = (jwt) => {
+export const saveJwtToLocal = (jwt: string) => {
   try {
     localStorage.setItem(jwtKey, jwt);
-  } catch (error) {
+  } catch (error: any) {
     console.log(error.message);
   }
 };
@@ -36,18 +43,18 @@ export const saveJwtToLocal = (jwt) => {
 export const signout = () => {
   try {
     localStorage.removeItem(jwtKey);
-  } catch (error) {
+  } catch (error: any) {
     console.log(error.message);
   }
 };
 
-export function getPost(postId) {
+export function getPost(postId: string): Promise<Response> {
   const res = fetch(`${API_ENDPOINT}/posts/${postId}`);
 
   return res;
 }
 
-export function getUserPosts(userId) {
+export function getUserPosts(userId: string): Promise<Response> {
   const res = fetch(`${API_ENDPOINT}/users/posts`, {
     headers: {
       Authorization,
@@ -57,11 +64,15 @@ export function getUserPosts(userId) {
   return res;
 }
 
-function getToken() {
+function getToken(): string | null {
   return localStorage.getItem(jwtKey);
 }
 
-export function createPost(title, body, publish = false) {
+export function createPost(
+  title: string,
+  body: string,
+  publish: boolean = false
+): Promise<Response> {
   return fetch(`${API_ENDPOINT}/posts`, {
     method: 'POST',
     headers: {
@@ -72,7 +83,11 @@ export function createPost(title, body, publish = false) {
   });
 }
 
-export function postComment(postId, name, comment) {
+export function postComment(
+  postId: string,
+  name: string,
+  comment: string
+): Promise<Response> {
   return fetch(`${API_ENDPOINT}/posts/${postId}/comments`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -80,10 +95,27 @@ export function postComment(postId, name, comment) {
   });
 }
 
-export function getComments(postId) {
+export function getComments(postId: string): Promise<Response> {
   return fetch(`${API_ENDPOINT}/posts/${postId}/comments`);
 }
 
-export function formatDate(date) {
+export function formatDate(date: string): string {
   return moment(date).format('MMM d, YYYY @ HH:mm');
+}
+
+export function updatePost(
+  postId: string,
+  title: string | null = null,
+  body: string | null = null,
+  published: boolean | null = null
+): Promise<Response> {
+  return fetch(`${API_ENDPOINT}/posts/${postId}`, {
+    method: 'put',
+    headers: { Authorization, 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title,
+      body,
+      published,
+    }),
+  });
 }

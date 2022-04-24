c367e112a072f816364d193b913bba9454b5d85c
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-25 03:13:08 +0900

    Convert to mui components

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,33 +1,14 @@
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from '@emotion/react';
+import { Backdrop, CircularProgress } from '@mui/material';
 import { useState } from 'react';
 
 const Loading = () => {
   const [isLoading, setLoading] = useState(false);
   const component = isLoading ? (
-    <div
-      css={css`
-        position: absolute;
-        display: grid;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        z-index: 1;
-        transition-property: background-color;
-        transition-duration: 300ms;
-        background-color: #aaaaaa45;
-      `}
-    >
-      <img
-        src="/images/loading-blocks.svg"
-        alt="Loading"
-        width={75}
-        height={75}
-      />
-    </div>
+    <Backdrop open={isLoading} sx={{ zIndex: 10 }}>
+      <CircularProgress color="secondary" />
+    </Backdrop>
   ) : null;
 
   return { setLoading, component };
